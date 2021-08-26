8eff7e97197d5441e3179c303c943fd05539a04f
Pedro <pmapm@ua.pt>
2021-08-26 16:38:41 +0100

    more jQueryUI examples

--- examples/jQueryUI/js/script.js
+++ examples/jQueryUI/js/script.js
@@ -1,7 +1,33 @@
 $(function(){
     $('.box').draggable();
-    $('#box1').draggable({ scroll: true });
+    $('#box1').draggable({ scroll: true, revert: "invalid" });
     $('#box2').draggable({ axis: "x"});
     $('#box3').draggable({ axis: "y"});
-    $('#box4').draggable({ containment: ".container"});
+    $('#box4').draggable({ containment: ".container", revert: "invalid"});
+
+    
+    $('#droppable').droppable({
+        accept: '#box1',
+        drop: function () {
+            $(this).text("When a box got attitude, drop it like it's hot!");
+          }
+    });
+
+    $('#sortable').sortable({connectWith:"#sortableToo", placeholder:"placeholderBox"});
+    $('#sortableToo').sortable({connectWith:"#sortable", placeholder:"placeholderBox"});
+
+
+    $('#accordion').accordion({collapsible: true, heightStyle:"content"});
+
+
+    $('.date').datepicker({
+        showOtherMonths: true,
+        selectOtherMonths:true,
+        showButtonPanel: true,
+        changeMonth: false,
+        changeYear: false,
+        numberOfMonths: 2,
+        minDate: 0 /*can't chosse nothing in the past (other values: -1D, -2W)*/ 
+    });
+
 });
