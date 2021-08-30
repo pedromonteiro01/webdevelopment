0f52906eb57fc63cb8f541b5ba2766e369b06076
Pedro <pmapm@ua.pt>
2021-08-30 16:02:41 +0100

    non-static website project using php

---
+++ projects/Non-StaticWebsite/includes/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Basic Website</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+        <style>
+            .bd-placeholder-img {
+              font-size: 1.125rem;
+              text-anchor: middle;
+              -webkit-user-select: none;
+              -moz-user-select: none;
+              user-select: none;
+            }
+      
+            @media (min-width: 768px) {
+              .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+              }
+            }
+          </style>
+        
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
---
+++ projects/Non-StaticWebsite/includes/posts.php
@@ -0,0 +1,57 @@
+<?php
+    // create an array of blog posts
+    $posts = array(
+        array(
+            "post-title" => "Sample Blog Post",
+            "post-date" => "January 1, 2021",
+            "post-author" => "Mark",
+            "post-content" => "<p>Sed faucibus posuere mi non bibendum. Ut rhoncus lacus ipsum, suscipit sollicitudin purus pretium 
+            nec. Cras massa erat, maximus non consectetur non, cursus id elit. Praesent condimentum enim ac pretium elementum. Curabitur pharetra nulla diam, sed posuere diam tristique quis. Vestibulum enim nulla, semper at cursus id, varius id urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer maximus condimentum lectus, sit amet bibendum quam dignissim eu. Sed vitae neque quis magna feugiat cursus id in dui.
+             Morbi non lacinia diam, eu tincidunt massa. Vestibulum in sodales libero.
+            </p>
+            <p>Sed faucibus posuere mi non bibendum. Ut rhoncus lacus ipsum, suscipit sollicitudin purus pretium 
+            nec. Cras massa erat, maximus non consectetur non, cursus id elit. Praesent condimentum enim ac pretium elementum. Curabitur pharetra nulla diam, sed posuere diam tristique quis. Vestibulum enim nulla, semper at cursus id, varius id urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer maximus condimentum lectus, sit amet bibendum quam dignissim eu. Sed vitae neque quis magna feugiat cursus id in dui.
+             Morbi non lacinia diam, eu tincidunt massa. Vestibulum in sodales libero.
+            </p>
+            <p>Sed faucibus posuere mi non bibendum. Ut rhoncus lacus ipsum, suscipit sollicitudin purus pretium 
+            nec. Cras massa erat, maximus non consectetur non, cursus id elit. Praesent condimentum enim ac pretium elementum. Curabitur pharetra nulla diam, sed posuere diam tristique quis. Vestibulum enim nulla, semper at cursus id, varius id urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer maximus condimentum lectus, sit amet bibendum quam dignissim eu. Sed vitae neque quis magna feugiat cursus id in dui.
+             Morbi non lacinia diam, eu tincidunt massa. Vestibulum in sodales libero.
+            </p>"
+            
+        ),
+        array(
+            "post-title" => "Another Blog Post",
+            "post-date" => "December 24, 2021",
+            "post-author" => "Jacob",
+            "post-content" => "<p>Sed faucibus posuere mi non bibendum. Ut rhoncus lacus ipsum, suscipit sollicitudin purus pretium 
+            nec. Cras massa erat, maximus non consectetur non, cursus id elit. Praesent condimentum enim ac pretium elementum. Curabitur pharetra nulla diam, sed posuere diam tristique quis. Vestibulum enim nulla, semper at cursus id, varius id urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer maximus condimentum lectus, sit amet bibendum quam dignissim eu. Sed vitae neque quis magna feugiat cursus id in dui.
+             Morbi non lacinia diam, eu tincidunt massa. Vestibulum in sodales libero.
+            </p>
+            <p>Sed faucibus posuere mi non bibendum. Ut rhoncus lacus ipsum, suscipit sollicitudin purus pretium 
+            nec. Cras massa erat, maximus non consectetur non, cursus id elit. Praesent condimentum enim ac pretium elementum. Curabitur pharetra nulla diam, sed posuere diam tristique quis. Vestibulum enim nulla, semper at cursus id, varius id urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer maximus condimentum lectus, sit amet bibendum quam dignissim eu. Sed vitae neque quis magna feugiat cursus id in dui.
+             Morbi non lacinia diam, eu tincidunt massa. Vestibulum in sodales libero.
+            </p>
+            <p>Sed faucibus posuere mi non bibendum. Ut rhoncus lacus ipsum, suscipit sollicitudin purus pretium 
+            nec. Cras massa erat, maximus non consectetur non, cursus id elit. Praesent condimentum enim ac pretium elementum. Curabitur pharetra nulla diam, sed posuere diam tristique quis. Vestibulum enim nulla, semper at cursus id, varius id urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer maximus condimentum lectus, sit amet bibendum quam dignissim eu. Sed vitae neque quis magna feugiat cursus id in dui.
+             Morbi non lacinia diam, eu tincidunt massa. Vestibulum in sodales libero.
+            </p>"
+        ),
+        array(
+            "post-title" => "New Feature",
+            "post-date" => "January 1, 2021",
+            "post-author" => "Chris",
+            "post-content" => "<p>Sed faucibus posuere mi non bibendum. Ut rhoncus lacus ipsum, suscipit sollicitudin purus pretium 
+            nec. Cras massa erat, maximus non consectetur non, cursus id elit. Praesent condimentum enim ac pretium elementum. Curabitur pharetra nulla diam, sed posuere diam tristique quis. Vestibulum enim nulla, semper at cursus id, varius id urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer maximus condimentum lectus, sit amet bibendum quam dignissim eu. Sed vitae neque quis magna feugiat cursus id in dui.
+             Morbi non lacinia diam, eu tincidunt massa. Vestibulum in sodales libero.
+            </p>
+            <p>Sed faucibus posuere mi non bibendum. Ut rhoncus lacus ipsum, suscipit sollicitudin purus pretium 
+            nec. Cras massa erat, maximus non consectetur non, cursus id elit. Praesent condimentum enim ac pretium elementum. Curabitur pharetra nulla diam, sed posuere diam tristique quis. Vestibulum enim nulla, semper at cursus id, varius id urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer maximus condimentum lectus, sit amet bibendum quam dignissim eu. Sed vitae neque quis magna feugiat cursus id in dui.
+             Morbi non lacinia diam, eu tincidunt massa. Vestibulum in sodales libero.
+            </p>
+            <p>Sed faucibus posuere mi non bibendum. Ut rhoncus lacus ipsum, suscipit sollicitudin purus pretium 
+            nec. Cras massa erat, maximus non consectetur non, cursus id elit. Praesent condimentum enim ac pretium elementum. Curabitur pharetra nulla diam, sed posuere diam tristique quis. Vestibulum enim nulla, semper at cursus id, varius id urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer maximus condimentum lectus, sit amet bibendum quam dignissim eu. Sed vitae neque quis magna feugiat cursus id in dui.
+             Morbi non lacinia diam, eu tincidunt massa. Vestibulum in sodales libero.
+            </p>"
+        )
+    );
+?>
